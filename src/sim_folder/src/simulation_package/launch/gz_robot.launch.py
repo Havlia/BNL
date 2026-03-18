0fd862a31640eee36e270e8219cb593cd8db3a41
Havlia <haavaali@stud.ntnu.no>
2026-03-18 15:58:37 +0100

    FØRSTE COMMIT PÅ LENG, yolact og wrapper funke fint.

--- src/sim_folder/src/simulation_package/launch/gz_robot.launch.py
+++ src/sim_folder/src/simulation_package/launch/gz_robot.launch.py
@@ -4,44 +4,73 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
-from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription, TimerAction
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription, TimerAction, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-
+import os
 
 def generate_launch_description():
     ros_gz_sim_pkg_path = get_package_share_directory('ros_gz_sim')
     sim_pkg_path = FindPackageShare('simulation_package')
     gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
 
+
+    startup_node = Node(
+        package='yolact_package',
+        executable='bnl_startup.sh',
+        name='BNL_startup',
+        output='screen',
+        emulate_tty=True
+
+    )
+
+    # Bridging and remapping Gazebo topics to ROS 2 (replace with your own topics)
+    bridge_node = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/scan_gpu@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
+                    '/camera_image@sensor_msgs/msg/Image[gz.msgs.Image'],
+        output='screen'
+    )
+    
+    wrapper_node = Node(
+        package='yolact_package',
+        executable='env_wrapper.sh',
+        name='ros_yolact_node',
+        output='screen'
+
+    )
+
+    gz_start_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gz_launch_path),
+        launch_arguments={
+            'gz_args': PathJoinSubstitution([sim_pkg_path, 'gazebo_includes', 'worlds/example.sdf']), 
+            'on_exit_shutdown': 'True'
+        }.items()
+    )
+
     return LaunchDescription([
         SetEnvironmentVariable(
             'GZ_SIM_RESOURCE_PATH',
             PathJoinSubstitution([sim_pkg_path, 'gazebo_includes', 'models'])
         ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gz_launch_path),
-            launch_arguments={
-                'gz_args': PathJoinSubstitution([sim_pkg_path, 'gazebo_includes', 'worlds/example.sdf']), 
-                'on_exit_shutdown': 'True'
-            }.items(),
+        SetEnvironmentVariable(
+            'BNL_VENV_PATH',os.path.expanduser('~/.BNL_venv/.venv') 
+        ),
+        SetEnvironmentVariable(
+            'CUDA_VISIBLE_DEVICES',""
         ),
 
-        # Bridging and remapping Gazebo topics to ROS 2 (replace with your own topics)
-        Node(
-            package='ros_gz_bridge',
-            executable='parameter_bridge',
-            arguments=['/scan_gpu@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
-                       '/camera_image@sensor_msgs/msg/Image[gz.msgs.Image'],
-            output='screen'
-        ),
+        startup_node,
 
-        Node(
-            package='nodes',
-            executable='print_exec',
-            name='test_node',
-            output='screen'
-
+        RegisterEventHandler(
+            OnProcessExit(
+                target_action=startup_node,
+                on_exit=[gz_start_node,
+                         bridge_node,
+                         wrapper_node]
+            )
         )
     ])
--- src/sim_folder/src/utilities/setup.py
+++ src/sim_folder/src/utilities/setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages, setup
-from glob import glob
 
-package_name = 'simulation_package'
+package_name = 'utilities'
 
 setup(
     name=package_name,
@@ -11,11 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/'+ package_name + '/launch', glob('launch/*.launch.py')),
-        ('share/' + package_name + '/gazebo_includes/models', glob('gazebo_includes/models/**/*', recursive=True)),
-        ('share/' + package_name + '/gazebo_includes/plugins', glob('gazebo_includes/plugins/**/*', recursive=True)),
-        ('share/' + package_name + '/gazebo_includes/worlds', glob('gazebo_includes/worlds/**/*', recursive=True)),
-        #('share/' + package_name + '/environment', ['scripts/env_hook.sh']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
